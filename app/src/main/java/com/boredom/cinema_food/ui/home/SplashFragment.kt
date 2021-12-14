77c8cdda0944f6f005bc3397d0736bb8f665441e
yudisdwi <yudisdwi02@gmail.com>
2021-12-15 03:44:29 +0700

    Make OnBoarding Pages and Layouting Profile

---
+++ app/src/main/java/com/boredom/cinema_food/ui/home/SplashFragment.kt
@@ -0,0 +1,21 @@
+package com.boredom.cinema_food.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.boredom.cinema_food.R
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+
+}
